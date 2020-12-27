29c43036d174e4018fba467c0b786d518dd9cc18
shafeekzamzam <shafeekzamzam@users.noreply.github.com>
2020-12-28 02:24:12 +0530

    Update CuckooQuickSetup.sh

--- CuckooQuickSetup.sh
+++ CuckooQuickSetup.sh
@@ -81,18 +81,19 @@ sudo apt-get install virtualbox-ext-pack     #Relevance??to be checked
 #VBoxManage goes here......
 #Assuming win7.iso is available for installation
 #========
+MACHINENAME="cuckoo1"
 #Create VM
-VBoxManage createvm --name $MACHINENAME --ostype "Debian_64" --register --basefolder `pwd`
+VBoxManage createvm --name $MACHINENAME --ostype "Windows 7 (64-bit)" --register --basefolder `pwd`
 #Set memory and network
 VBoxManage modifyvm $MACHINENAME --ioapic on
-VBoxManage modifyvm $MACHINENAME --memory 1024 --vram 128
+VBoxManage modifyvm $MACHINENAME --memory 4096 --vram 128
 VBoxManage modifyvm $MACHINENAME --nic1 nat
 #Create Disk and connect Debian Iso
 VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi --size 80000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
-VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/debian.iso
+VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/win7ultimate.iso
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
 #=====
 
