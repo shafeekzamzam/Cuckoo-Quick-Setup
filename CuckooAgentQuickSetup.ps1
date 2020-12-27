c00b5b175291ed3b4dad689e17ec248f42d4df2f
shafeekzamzam <shafeekzamzam@users.noreply.github.com>
2020-12-27 13:35:56 +0530

    Update CuckooAgentQuickSetup.ps1

--- CuckooAgentQuickSetup.ps1
+++ CuckooAgentQuickSetup.ps1
@@ -1,5 +1,8 @@
 #agent install in win7x64Cuckoo
 
+#ISSUE- Powershell2.0 doesnt support INvoke-webrequest
+#Solution - Download all requied installers in ubuntu host and share the folder to win7 & use older ps script to install them.
+
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/2.7.14/python-2.7.14.amd64.msi" -OutFile "c:/temp/python-2.7.14.amd64.msi"
 Invoke-WebRequest -Uri "https://redirector.gvt1.com/edgedl/release2/chrome/VM7AZGvxTD3eRDzvxYLSvg_86.0.4240.75/86.0.4240.75_chrome_installer.exe" -OutFile "c:/temp/86.0.4240.75_chrome_installer.exe"
 Invoke-WebRequest -Uri "ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1901020099/AcroRdrDC1901020099_en_US.exe" -OutFile "c:/temp/AcroRdrDC1901020099_en_US.exe"
